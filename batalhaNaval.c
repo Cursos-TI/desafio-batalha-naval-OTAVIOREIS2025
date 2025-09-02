a2a02ac39b9b61b9c19f7efae85bd8113fa0b4d7
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2025-09-02 23:19:22 +0000

    matriz bidimensional para representar o tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
+    const int TAM = 10;  
+    int tabuleiro[TAM][TAM];
+}
+
+ // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
@@ -36,5 +38,3 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
