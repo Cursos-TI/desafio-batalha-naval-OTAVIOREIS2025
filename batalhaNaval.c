b01d65b5a7fbcd7cdc5990606404d1bf0ab7392c
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2025-09-05 03:02:02 +0000

    Navio na horizontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,6 +8,10 @@
 int main() {
     const int TAM = 10;
     int tabuleiro[TAM][TAM] = {0}; // inicializa com zeros
-   
+
+    // Navio horizontal (linha 5, colunas C-E)
+    for (int j = 2; j < 5; j++) {
+        tabuleiro[4][j] = 3;
+    }
 
     
