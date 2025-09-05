efe796b9cd94651aa8962679a57c757bf878d981
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2025-09-05 03:05:06 +0000

    Coordenadas do navio na horizontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,5 +18,12 @@ int main() {
     for (int i = 2; i < 5; i++) {
         tabuleiro[i][6] = 3;
     }
+    
+    // Coordenadas navio horizontal
+    printf("Coordenadas do navio horizontal (3 posições):\n");
+    for (int j = 2; j < 5; j++) {
+        printf("(Linha %d, Coluna %c)\n", 5, 'A' + j);
+    }
+
 
     
